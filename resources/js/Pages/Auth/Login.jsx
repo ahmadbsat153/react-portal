7af9c84c8b23663d1a50b821931cce7aa2a8103a
ahmadbsat153 <ahmadb@gtls.com.au>
2023-08-21 14:52:21 +0300

    Pushed

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -12,7 +12,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 import ReCAPTCHA from "react-google-recaptcha";
 import { InertiaApp } from "@inertiajs/inertia-react";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
-import "../../../css/scroll.css";
+import "../../../css/scroll.css"
 import axios from "axios";
 const msalConfig = {
     auth: {
@@ -96,43 +96,43 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <div className="bg-black">
+            <div className="bg-black">
             <GuestLayout>
                 <Head title="Sign in" />
                 <div className="flex  flex-col justify-center items-center ">
                     <div className=" w-full shadow-md rounded px-8 pt-6 pb-8 mb-4 relative">
                         <form onSubmit={submit} className="space-y-4">
-                            <div className="mt-1">
-                                <a
-                                    href="/"
-                                    className="text-white hover:text-goldd flex items-center"
+                        <div className="mt-1">
+                            <a
+                                href="/"
+                                className="text-white hover:text-goldd flex items-center"
+                            >
+                                <svg
+                                    viewBox="0 0 64 64"
+                                    fill="currentColor"
+                                    height="1.3em"
+                                    width="1.3em"
                                 >
-                                    <svg
-                                        viewBox="0 0 64 64"
-                                        fill="currentColor"
-                                        height="1.3em"
-                                        width="1.3em"
-                                    >
-                                        <path
-                                            fill="none"
-                                            stroke="currentColor"
-                                            strokeLinejoin="bevel"
-                                            strokeMiterlimit={10}
-                                            strokeWidth={2}
-                                            d="M32.936 48.936l-17-17 17-17"
-                                        />
-                                        <path
-                                            fill="none"
-                                            stroke="currentColor"
-                                            strokeLinejoin="bevel"
-                                            strokeMiterlimit={10}
-                                            strokeWidth={2}
-                                            d="M47.936 48.936l-17-17 17-17"
-                                        />
-                                    </svg>{" "}
-                                    Back to home
-                                </a>
-                            </div>
+                                    <path
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeLinejoin="bevel"
+                                        strokeMiterlimit={10}
+                                        strokeWidth={2}
+                                        d="M32.936 48.936l-17-17 17-17"
+                                    />
+                                    <path
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeLinejoin="bevel"
+                                        strokeMiterlimit={10}
+                                        strokeWidth={2}
+                                        d="M47.936 48.936l-17-17 17-17"
+                                    />
+                                </svg>{" "}
+                                Back to home
+                            </a>
+                        </div>
                             <div className="relative">
                                 <InputLabel
                                     htmlFor="email"
@@ -155,7 +155,19 @@ export default function Login({ status, canResetPassword }) {
                                         className={`  appearance-none mb-2 border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline transition-all duration-500`}
                                         placeholder="Email"
                                     />
-
+                                    {/* <PasswordInput
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        value={data.password}
+                                        autoComplete="current-password"
+                                        onChange={handleOnChange}
+                                        className={`${
+                                            showPassword
+                                                ? "opacity-100 block"
+                                                : "opacity-0 translate-x-full max-h-0"
+                                        } appearance-none w-full border mb-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline transition-all duration-500`}
+                                    /> */}
                                     <div className="relative">
                                         <InputLabel
                                             htmlFor="password"
@@ -180,23 +192,23 @@ export default function Login({ status, canResetPassword }) {
                                                 <EyeSlashIcon
                                                     className={` w-4 h-4`}
                                                 />
-                                            ) : (
-                                                <EyeIcon
+                                            ) : (<EyeIcon
                                                     className={` w-4 h-4`}
                                                 />
+                                                
                                             )}
                                         </div>
                                     </div>
                                     <div className="flex items-center justify-end mt-0">
-                                        {canResetPassword && (
-                                            <Link
-                                                href={route("password.request")}
-                                                className="underline text-sm text-goldd dark:text-smooth hover:text-gray-900 dark:hover:text-goldd rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                                            >
-                                                Forgot your password?
-                                            </Link>
-                                        )}
-                                    </div>
+                            {canResetPassword && (
+                                <Link
+                                    href={route("password.request")}
+                                    className="underline text-sm text-goldd dark:text-smooth hover:text-gray-900 dark:hover:text-goldd rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                >
+                                    Forgot your password?
+                                </Link>
+                            )}
+                        </div>
                                     <InputError
                                         message={errors.email}
                                         className="mt-2"
@@ -207,7 +219,15 @@ export default function Login({ status, canResetPassword }) {
                                     />
                                 </div>
                             </div>
+                            
                             <div className="flex items-center justify-between">
+                                {/* <PrimaryButton
+                                    disabled={processing || !recaptchaValue}
+                                    className={` flex w-full justify-center rounded-md border border-transparent bg-goldd py-2 px-4 text-sm font-medium text-dark shadow-sm hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
+                                    type="submit"
+                                >
+                                    Sign In
+                                </PrimaryButton> */}
                                 <button
                                     className={`flex w-full justify-center ${
                                         processing || !recaptchaValue
@@ -229,12 +249,80 @@ export default function Login({ status, canResetPassword }) {
                             size="normal" // Set the desired size here: "compact", "normal", or "invisible"
                             render="explicit" // Use "explicit" rendering
                             theme="dark" // Set the desired theme: "light" or "dark"
-                            style={{ transform: "scale(0.8)" }} // Use CSS transform to adjust the size
+                            style={{ transform: 'scale(0.8)' }} // Use CSS transform to adjust the size
                         />
+                        
+                        
                     </div>
                 </div>
-               
+                {/* <form onSubmit={submit} className="space-y-6">
+                    <div>
+                        <InputLabel
+                            htmlFor="email"
+                            value="Email"
+                            className="block text-sm font-medium text-white"
+                        />
+
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            autoComplete="username"
+                            isFocused={true}
+                            onChange={handleOnChange}
+                        />
+
+                        <InputError message={errors.email} className="mt-2" />
+                    </div>
+
+                    <div className="mt-4">
+                        <InputLabel
+                            htmlFor="password"
+                            value="Password"
+                            className="block text-sm font-medium text-white"
+                        />
+
+                        <TextInput
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            autoComplete="current-password"
+                            onChange={handleOnChange}
+                        />
+
+                        <InputError
+                            message={errors.password}
+                            className="mt-2"
+                        />
+                        {canResetPassword && (
+                            <Link
+                                href={route("password.request")}
+                                className="mt-4 font-medium text-yellow-600 hover:text-yellow-500 "
+                            >
+                                Forgot your password ? 
+                            </Link>
+                        )}
+                    </div>
+
+                    
+
+                    <div className="flex flex-col items-center justify-between">
+                        
+
+                        <PrimaryButton
+                            className=" flex w-full justify-center rounded-md border border-transparent bg-yellow-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            disabled={processing}
+                        >
+                            SIGN IN
+                        </PrimaryButton>
+                    </div>
+                </form> */}
             </GuestLayout>
-        </div>
+            </div>
+        
     );
 }
